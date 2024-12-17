54699cda581f415be5efc9ecdd54177d2d6f9d14
yarden_itzhaky <yardene015@gmail.com>
2024-12-17 14:31:43 +0200

    fix

--- server/startup.sh
+++ server/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-gunicorn --bind=0.0.0.0 --timeout 30 --workers=2 --threads=2 app:app
+echo "gunicorn --bind=0.0.0.0:8000 --timeout=30 --workers=2 --threads=2 app:app" > startup.sh
