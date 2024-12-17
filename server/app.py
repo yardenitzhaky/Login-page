befa6eeacfae7692fb0260c9aafba2b021d25607
yarden_itzhaky <yardene015@gmail.com>
2024-12-17 14:20:07 +0200

    few more fixes

--- server/app.py
+++ server/app.py
@@ -144,4 +144,4 @@ def register():
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 8000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
--- server/startup.sh
+++ server/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-gunicorn --bind=0.0.0.0 --timeout 600 app:app
+gunicorn --bind=0.0.0.0 --timeout 30 --workers=2 --threads=2 app:app
