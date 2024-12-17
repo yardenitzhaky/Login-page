ce23d00011ccc945998cbf1af8a5d8cd9a56b716
yarden_itzhaky <yardene015@gmail.com>
2024-12-17 14:45:06 +0200

    ff

--- server/app.py
+++ server/app.py
@@ -144,4 +144,4 @@ def register():
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 8000))
-    #app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port, debug=False)
