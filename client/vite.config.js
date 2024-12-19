27986c7e73fe9baea8b1339511c5cd9e4087a49a
yarden_itzhaky <yardene015@gmail.com>
2024-12-19 15:24:30 +0200

    Setup GitHub Pages deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Login-page/',  // Replace REPO_NAME with your repository name
   server: {
     port: 3000,
     proxy: {
